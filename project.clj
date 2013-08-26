1175c55fa5be09a6050aaa2fbeafd42a4085c144
Ulises Cervino Beresi <ulises@cloudant.com>
2013-08-26 21:31:18 +0100

    Add project options.

--- project.clj
+++ project.clj
@@ -1,6 +1,18 @@
 (defproject clojunauts/clojunauts "0.1.0-SNAPSHOT"
-  :description "TODO"
-  :url "TODO"
+  :description "Nothing yet."
+
+  :url "http://www.clojunauts.com/"
+
+  :min-lein-version "2.0.0"
+
   :dependencies [[org.clojure/clojure "1.5.1"]]
+
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
-                   :source-paths ["dev"]}})
+                   :source-paths ["dev"]}}
+
+  :source-paths ["src/clj"]
+
+  :uberjar-name "clojunauts-standalone.jar"
+  :main clojurians.app
+
+  :aot :all)
