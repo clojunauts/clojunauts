2ab36ed540ece7f54aa7b275a155b08e01709314
Ulises Cervino Beresi <ulises@cloudant.com>
2013-08-26 21:04:40 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# clojunauts/clojunauts
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clojunauts/clojunauts "0.1.0-SNAPSHOT"
+  :description "TODO"
+  :url "TODO"
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
+                   :source-paths ["dev"]}})
