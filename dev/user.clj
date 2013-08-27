b8bb7630b4ffd50c7186dae3f675bcb1778b7f4b
Ulises Cervino Beresi <ulises@cloudant.com>
2013-08-27 20:11:27 +0100

    Convenience functions for resetting a running app.

--- dev/user.clj
+++ dev/user.clj
@@ -11,7 +11,7 @@
    [clojure.string :as str]
    [clojure.test :as test]
    [clojure.tools.namespace.repl :refer (refresh refresh-all)]
-   [clojunauts.core]))
+   [clojunauts.core :as clojunauts]))
 
 (def system
   "A Var containing an object representing the application under
@@ -22,21 +22,20 @@
   "Creates and initializes the system under development in the Var
   #'system."
   []
-  ;; TODO
-  )
+  (alter-var-root
+   #'system
+   (constantly (clojunauts/app {}))))
 
 (defn start
   "Starts the system running, updates the Var #'system."
   []
-  ;; TODO
-  )
+  (alter-var-root #'system clojunauts/start))
 
 (defn stop
   "Stops the system if it is currently running, updates the Var
   #'system."
   []
-  ;; TODO
-  )
+  (alter-var-root #'system clojunauts/stop))
 
 (defn go
   "Initializes and starts the system running."
