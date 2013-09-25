6ac6553ece29e069c1b9ce4179fe1466e910b3f2
Fabián Ezequiel Gallina <galli.87@gmail.com>
2013-09-25 10:44:58 -0300

    Added migratus

--- project.clj
+++ project.clj
@@ -11,11 +11,29 @@
                  [korma "0.3.0-RC5"]
                  [clojunauts/postgresql "9.2-1003.jdbc4"]
                  [clojunauts/postgis-jdbc "2.1.0SVN"]
+                 [migratus "0.6.0"]
                  [environ "0.4.0"]]
+
+  :plugins [[migratus-lein "0.1.0"]]
 
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]
                                   [clj-http "0.7.6"]]
                    :source-paths ["dev"]}}
+
+  :migratus {:store :database
+             :migration-dir "migrations"
+             :db {:classname "org.postgis.DriverWrapper"
+                  :subprotocol "postgresql"
+                  :subname
+                  ~(str
+                    "//"
+                    (get (System/getenv) "CLOJUNAUTS_DB_HOST" "localhost")
+                    ":"
+                    (get (System/getenv) "CLOJUNAUTS_DB_PORT" "5432")
+                    "/"
+                    (get (System/getenv) "CLOJUNAUTS_DB_NAME" "clojunauts"))
+                  :user (get (System/getenv) "CLOJUNAUTS_DB_USER" "clojunauts")
+                  :password (get (System/getenv) "CLOJUNAUTS_DB_PASSWORD" "")}}
 
   :source-paths ["src/clj"]
 
